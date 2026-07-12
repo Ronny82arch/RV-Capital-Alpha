39699b1bcfeefd19a7c77f52d7269dc259206260
Ronny82arch <ronnyvisentin@hotmail.it>
2026-07-12 16:53:01 +0200

    Clean up: Delete gv-capital-alpha/build_test.ps1

--- gv-capital-alpha/build_test.ps1
+++
@@ -1,8 +0,0 @@
-$env:Path += ";C:\Users\Ronny Visentin\AppData\Local\Temp\node\node-v20.14.0-win-x64"
-Write-Host "Node version:"
-node -v
-Write-Host "NPM version:"
-npm -v
-
-Write-Host "Running npm run build..."
-npm run build
